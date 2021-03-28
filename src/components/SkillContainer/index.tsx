cd5b190a9f600f4cff089c55acbb7249869ea838
Adriano Heller Mylla <adriano@technoizz.com>
2021-03-28 20:44:21 -0300

    feat: adjusting component to receive dynamic data

--- src/components/SkillContainer/index.tsx
+++ src/components/SkillContainer/index.tsx
@@ -7,15 +7,35 @@ import {
  } from './styles';
 
 interface IChildProps{
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    data: {
+        type?: string,
+        skill: string
+    }[]
 };
 
-const SkillContainer:React.FC<IChildProps> = ({children}) => {
+const SkillContainer:React.FC<IChildProps> = ({ children,data }) => {
     return(
         <Container>
             <ContainerHeader>
                 { children }
             </ContainerHeader>
+                {
+                    !!data && data.length > 0 ?
+                        data.map((item,index) => {
+                            if(item['type']){
+                                return(
+                                    <>
+                                        <h2 key={index}>{item['type']}</h2>
+                                        <p key={index}>{item['skill']}</p>
+                                    </>
+                                )
+                            }else{
+                                <p key={index}>{item['skill']}</p>
+                            };
+                        }) :
+                        <p>Loading Data...</p>
+                }
         </Container>
     )
 };
--- src/components/SkillContainer/styles.ts
+++ src/components/SkillContainer/styles.ts
@@ -22,3 +22,4 @@ export const ContainerBody = styled.div`
     justify-content: center;
     height: 100%;
 `;
+
